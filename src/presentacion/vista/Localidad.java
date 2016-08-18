490a1dc0ef2c0eaed91229f9de1078c66365f2bf
e146506 <e146506@giitmoni123.anses.gov.ar>
2016-08-18 18:03:28 -0300

    Se agregan ventanas Localidad y contacto.

---
+++ src/presentacion/vista/Localidad.java
@@ -0,0 +1,81 @@
+package presentacion.vista;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
+
+import presentacion.controlador.Controlador;
+
+
+public class Localidad extends JFrame
+{
+	private static final long serialVersionUID = 1L;
+	private JFrame frame;
+	private JPanel contentPane;
+	private JTable tablaPersonas;
+	private JButton btnAgregar;
+	private JButton btnEditar;
+	private DefaultTableModel modelPersonas;
+	private Controlador controlador;
+	private  String[] nombreColumnas = {"Nombre y apellido","Teléfono", "Direccion", "NroCalle", "NroPiso" ,"NroDpto", "Localidad", "Mail", "Fecha de Cumpleaños", "Contacto"};
+	
+	public Localidad(Controlador controlador) 
+	{
+		super();
+		this.setControlador(controlador);
+		
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setBounds(100, 100, 710, 470);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		frame = new JFrame();
+		frame.setBounds(100, 100, 823, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(0, 0, 797, 262);
+		frame.getContentPane().add(panel);
+		panel.setLayout(null);
+		
+		JScrollPane spPersonas = new JScrollPane();
+		spPersonas.setBounds(10, 11, 777, 182);
+		panel.add(spPersonas);
+		
+		modelPersonas = new DefaultTableModel(null,nombreColumnas);
+		tablaPersonas = new JTable(modelPersonas);
+		
+		tablaPersonas.getColumnModel().getColumn(0).setPreferredWidth(103);
+		tablaPersonas.getColumnModel().getColumn(0).setResizable(false);
+		tablaPersonas.getColumnModel().getColumn(1).setPreferredWidth(100);
+		tablaPersonas.getColumnModel().getColumn(1).setResizable(false);
+		
+		spPersonas.setViewportView(tablaPersonas);
+		
+		btnAgregar = new JButton("Agregar");
+		btnAgregar.setBounds(10, 228, 89, 23);
+		panel.add(btnAgregar);
+		
+		btnEditar = new JButton("Editar");
+		btnEditar.setBounds(109, 228, 89, 23);
+		panel.add(btnEditar);
+		
+		this.setVisible(true);
+	}
+
+	public Controlador getControlador() {
+		return controlador;
+	}
+
+	public void setControlador(Controlador controlador) {
+		this.controlador = controlador;
+	}
+	
+}
---
+++ src/presentacion/vista/ventanaContacto.java
@@ -0,0 +1,22 @@
+package presentacion.vista;
+
+import javax.swing.JFrame;
+
+import presentacion.controlador.Controlador;
+
+public class ventanaContacto  extends JFrame
+{
+	private static final long serialVersionUID = 1L;
+
+	public ventanaContacto(Controlador controlador) 
+	{
+		super();
+		initialize();
+	}
+
+	private void initialize() {
+		
+		
+		this.setVisible(true);
+	}
+}
